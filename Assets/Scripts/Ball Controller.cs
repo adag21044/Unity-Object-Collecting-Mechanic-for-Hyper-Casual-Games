9ac3ec73d18c8af320c3bfec506b405257b693d9
Ahmet E. Dag <adag21044@gmail.com>
2024-09-16 17:29:13 +0300

    Movement issue fixed

--- Assets/Scripts/Ball Controller.cs
+++ Assets/Scripts/Ball Controller.cs
@@ -2,36 +2,50 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    [SerializeField] private float horizontalSpeed;
-    [SerializeField] private float horizontalLimit;
+    [SerializeField] private float ZAxisSpeed;
+    [SerializeField] private float horizontalSpeed = 10f;
+    [SerializeField] private float horizontalLimit = 2.14f;
     private float horizontal;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool isDragging = false;  // Fareye basılıp sürüklenip sürüklenmediğini takip eder
+    private Vector3 lastMousePosition;
 
     // Update is called once per frame
     void Update()
     {
-        HorizantalMove();    
+        HorizontalMove();    
+        ForwardMove();
     }
 
-    private void HorizantalMove()
+    private void HorizontalMove()
     {
         float newX;
 
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0)) // Fareye basıldığında
         {
-            horizontal = Input.GetAxis("Mouse X");
+            isDragging = true;  // Sürükleme işlemi başlıyor
+            lastMousePosition = Input.mousePosition;  // İlk fare pozisyonunu kaydet
+        }
+        else if (Input.GetMouseButtonUp(0)) // Fare bırakıldığında
+        {
+            isDragging = false;  // Sürükleme işlemi bitiyor
         }
 
-        newX = transform.position.x + horizontal * horizontalSpeed * Time.deltaTime;
-        newX = Mathf.Clamp(newX, -horizontalLimit, horizontalLimit);
+        if (isDragging)
+        {
+            Vector3 currentMousePosition = Input.mousePosition;  // Şu anki fare pozisyonunu al
+            float deltaX = (currentMousePosition.x - lastMousePosition.x) * horizontalSpeed * Time.deltaTime;  // X eksenindeki fare hareketini al
 
-        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+            newX = transform.position.x + deltaX;
+            newX = Mathf.Clamp(newX, -horizontalLimit, horizontalLimit);  // X pozisyonunu sınırlandır
+
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);  // Yeni pozisyona taşı
+
+            lastMousePosition = currentMousePosition;  // Son fare pozisyonunu güncelle
+        }
     }
 
-    
+    private void ForwardMove()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * ZAxisSpeed);
+    }
 }
